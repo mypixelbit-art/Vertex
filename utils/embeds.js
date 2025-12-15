0f7227499a481f4e7a632fa8fb3916e93fbf7aa2
mypixelbit-art <mypixelbit@gmail.com>
2025-12-15 15:46:49 +0400

    Delete utils directory

--- utils/embeds.js
+++
@@ -1,9 +0,0 @@
-import { EmbedBuilder } from "discord.js";
-
-export function redEmbed(title, description) {
-  return new EmbedBuilder()
-    .setColor(0xE53935) // RED
-    .setTitle(title)
-    .setDescription(description)
-    .setTimestamp();
-}
